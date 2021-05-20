0a4ba691dc145fa7adad1c2d0fea2c2055683071
Josué Guardado <jguardadosv@outlook.com>
2021-05-20 08:37:03 -0600

    Agregar archivos de proyecto.

---
+++ Hilos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ImprimirY();
+            ImprimirX();
+
+        }
+
+        static void ImprimirY()
+        {
+            while (true)
+            {
+                Console.Write("Y");
+            }
+        }
+
+        static void ImprimirX()
+        {
+            while (true)
+            {
+                Console.Write("X");
+
+            }
+
+        }
+    }
+}
